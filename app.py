787aa6dbfd8e7fff614a1a06902b3abc7b040c7f
LagrinDev <45242069+lagrindev@users.noreply.github.com>
2026-04-01 00:11:03 +0300

    12

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
 from flask import Flask, request
-import requests
-import os
 
 app = Flask(__name__)
 
-TOKEN = os.getenv("TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
 @app.route('/')
 def home():
-    return "Bot is running"
+    return "OK"
 
 @app.route('/send', methods=['POST'])
 def send():
     data = request.json
-    text = data.get("text", "Нет данных")
-
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-    requests.post(url, json={
-        "chat_id": CHAT_ID,
-        "text": text
-    })
-
-    return {"status": "ok"}
+    return {"got": data}
